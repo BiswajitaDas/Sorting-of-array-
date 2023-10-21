cf5ee465923e637091298e08a8c08088a8eb1103
chinmay <cmohanty674@gmail.com>
2023-10-21 15:14:00 +0530

    sorting array

--- sortArray.js
+++ sortArray.js
@@ -1,3 +1,7 @@
-arr = [5, 2, 9, 1, 5, 6]
-sorted_arr = sorted(arr, reverse=True)
-print(sorted_arr)
+var arr = [5, 2, 9, 1, 5, 6];
+
+arr.sort(function(a, b) {
+  return b - a;
+});
+
+console.log(arr);
